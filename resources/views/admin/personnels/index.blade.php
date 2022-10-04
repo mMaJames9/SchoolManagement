12ed11a3fb80b54991bdc6c00e8a4ab76031a1da
mMaJames9 <anthonymisse85@gmail.com>
2022-10-04 14:50:34 +0100

    Push

--- resources/views/admin/personnels/index.blade.php
+++ resources/views/admin/personnels/index.blade.php
@@ -143,7 +143,7 @@
                                     <span class="fa fa-eye"  data-toogle="tooltip" data-placement="top" data-original-title="Afficher les informations de cet employé"></span>
                                 </a>
 
-                               @can('personnel.edit')
+                               @if(Auth::user()->roles->first()->name == 'Fondateur')
                                 <a href="{{ route('personnels.edit', $personnel->id) }}" class="btn btn-sm btn-info" data-target="#editPersonnel{{ $personnel->id }}" data-toggle="modal">
                                     <span class="fa fa-paste"  data-toogle="tooltip" data-placement="top" data-original-title="Modifier les informations de cet employé"></span>
                                 </a>
